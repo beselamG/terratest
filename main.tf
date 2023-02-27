19edd4c03bc6caf3ee780863f789d28692145b11
Beselam <beselam.asefa@nordcloud.com>
2023-02-27 22:37:23 +0200

    var

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 
 resource "azurerm_storage_account" "mystoraehacct12x43" {
   name                     = "mystoraehacct12x43"
-  resource_group_name      = "rg-beselam-aug-we"
+  resource_group_name      = var.resource_group_name
   location                 = "West Europe"
   account_tier             = "Standard"
   account_replication_type = "LRS"
